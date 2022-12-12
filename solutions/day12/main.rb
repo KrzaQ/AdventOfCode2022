28e4564405b22449e1afc5643c4464d58b7d10f2
krzaq <krzaq@krzaq.cc>
2022-12-12 17:50:06 +0100

    Day 12 cleanup.

--- solutions/day12/main.rb
+++ solutions/day12/main.rb
@@ -13,81 +13,60 @@ def letter_to_elevation(letter)
     end
 end
 
-def around(x, y)
-    [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].reject {
-        _1[0] < 0 || _1[1] < 0 || _1[01] >= DATA.size || _1[0] >= DATA[_1[1]].size }
-        .reject{ DATA.dig(*_1.reverse) != :start and DATA[y][x] != :start and
-        DATA.dig(*_1.reverse) != :end and DATA[y][x] != :end and
-        (DATA.dig(*_1.reverse) - DATA[y][x] > 1) }
+def dijkstra from, found:, next_nodes:, distance:
+    queue = [from]
+    distances = { from => 0 }
+    while queue.any?
+        cur = queue.shift
+        cur_dist = distances[cur]
+        return distances[cur] if found[cur]
+        next_nodes[cur].each do |next_node|
+            next_dist = distance[cur, next_node] + cur_dist
+            next if distances[next_node] and distances[next_node] <= next_dist
+            distances[next_node] = next_dist
+            queue << next_node
+        end
+        queue.sort_by!{ distances[_1] }
+    end
+    distances
+end
+
+def around x, y, xs, ys, diff_reject_func: ->(diff){ diff > 1 }
+    [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+        .select{ xs.include? _1[0] and ys.include? _1[1] }
+        .reject{ diff_reject_func[DATA.dig(*_1.reverse) - DATA.dig(y, x)] }
 end
 
 DATA = File.read('data.txt').lines.map do |line|
     line.strip.chars.map { letter_to_elevation(_1) }
 end
+S = DATA.find_index{ _1.include?(-1) }
+    .yield_self{ [_1, DATA[_1].find_index(-1)] }
+    .reverse
+E = DATA.find_index{ _1.include?(26) }
+    .yield_self{ [_1, DATA[_1].find_index(26)] }
+    .reverse
+DATA[S[1]][S[0]] = 0
+DATA[E[1]][E[0]] = ?z.ord - ?a.ord
 
-# S = DATA.find_index { _1.include?(:start) }.yield_self{ [_1, DATA[_1].find_index(:start)] }
-# E = DATA.find_index { _1.include?(:end) }.yield_self{ [_1, DATA[_1].find_index(:end)] }
-S = DATA.find_index { _1.include?(-1) }.yield_self{ [_1, DATA[_1].find_index(-1)] }.reverse
-E = DATA.find_index { _1.include?(26) }.yield_self{ [_1, DATA[_1].find_index(26)] }.reverse
-p [S, E]
-def dijkstra from, to
-    queue = [from]
-    distances = { from => 0 }
-    while queue.any?
-        current = queue.shift
-        current_distance = distances[current]
-        return distances[current] if current == to
-        if current == [2,4]
-            p current
-            p around(*current)
-            p DATA.dig(*current)
-            p DATA.dig(2, 5)
-        end
-        
-        around(*current).each do |next_node|
-            # p next_node if next_node == [2,4]
-            next if distances[next_node] && distances[next_node] <= current_distance + 1
-            distances[next_node] = current_distance + 1
-            queue << next_node
-        end
+def part1
+    next_nodes = ->(x){ around(x[0], x[1], 0...DATA[0].size, 0...DATA.size) }
+    distance = ->(_, _){ 1 }
+    found = ->(x){ x == E }
+    dijkstra(S, found: found, next_nodes: next_nodes, distance: distance)
+end
+
+def part2
+    reject = ->(diff){ diff < -1 }
+    next_nodes = ->(n) do
+        xs = 0...DATA[n[1]].size
+        ys = 0...DATA.size
+        around(n[0], n[1], xs, ys, diff_reject_func: reject)
     end
-    distances
+    distance = ->(_, _){ 1 }
+    found = ->(n){ DATA.dig(*n.reverse) == 0 }
+    dijkstra(E, next_nodes: next_nodes, distance: distance, found: found)
 end
 
-def around2(x, y)
-    [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].reject {
-        _1[0] < 0 || _1[1] < 0 || _1[1] >= DATA.size || _1[0] >= DATA[_1[1]].size }
-        .reject{ 
-            # p [DATA.dig(*_1.reverse), DATA[y][x]] 
-            (DATA.dig(*_1.reverse) - DATA[y][x] < -1) }
-end
-
-
-def dijkstra2 from
-    queue = [from]
-    distances = { from => 0 }
-    while queue.any?
-        current = queue.shift
-        current_distance = distances[current]
-        # p [current, DATA.dig(*current.reverse)]
-        # p around2(*current)
-        return distances[current] if [0, -1].include? DATA.dig(*current.reverse)
-
-        around2(*current).each do |next_node|
-            next if distances[next_node] && distances[next_node] <= current_distance + 1
-            distances[next_node] = current_distance + 1
-            queue << next_node
-        end
-    end
-    distances
-end
-
-puts 'Part 1: %s' % dijkstra(S, E)
-puts 'Part 2: %s' % dijkstra2(E)
-
-exit
-PART1 = DATA.last
-PART2 = DATA[-3..-1].sum
-
-puts 'Part 1: %s' % PART1
-puts 'Part 2: %s' % PART2
+puts 'Part 1: %s' % part1
+puts 'Part 2: %s' % part2
